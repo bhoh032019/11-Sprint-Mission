5f79558b75163ed00cbdcf776ffe2b18464e92d5
bhoh032019 <bhoh0320@ajou.ac.kr>
2024-12-06 00:41:23 +0900

    Feat: localStorage의 accessToken의 여부에 따른 navbar 변경

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -2,9 +2,27 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '@styles/Header.module.css';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
   const { pathname } = useRouter();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(false);
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    setIsCheckingAuth(false); // 로그아웃 후 상태 업데이트
+    setIsDropdownVisible(false); // 드롭다운 닫기
+  };
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    setIsCheckingAuth(!!accessToken); // accessToken 존재 여부에 따라 상태 설정
+  }, []);
 
   return (
     <>
@@ -26,7 +44,7 @@ const Header = () => {
               href="/items"
               className={
                 pathname === '/items' || pathname === '/additem'
-                  ? styles['active-link nav-list']
+                  ? `${styles['active-link']} ${styles['nav-list']}`
                   : styles['nav-list']
               }
             >
@@ -34,9 +52,32 @@ const Header = () => {
             </Link>
           </div>
         </div>
-        <Link className={styles['login']} href="/login">
-          로그인
-        </Link>
+        {isCheckingAuth ? (
+          <div className={styles['sortButtonWrapper']}>
+            <button
+              className={styles['sortDropdownTriggerButton']}
+              onClick={toggleDropdown}
+            >
+              <Image
+                src="/svgs/ic_profile.svg"
+                width={40}
+                height={40}
+                alt="프로필 이미지"
+              />
+            </button>
+            {isDropdownVisible && (
+              <div className={styles['dropdownMenu']}>
+                <div className={styles['dropdownItem']} onClick={handleLogout}>
+                  로그아웃
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Link className={styles['login']} href="/login">
+            로그인
+          </Link>
+        )}
       </nav>
     </>
   );
