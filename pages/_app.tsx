51b3d970cdb60b67f5e1ccd563a967aad41f54ba
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-01-18 19:34:52 +0900

    feat: react-query설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,26 @@ import { AppProps } from 'next/app';
 import Layout from '@components/Layout/Layout';
 import React from 'react';
 import { AuthProvider } from '@/contexts/AuthProvider';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   );
 }
